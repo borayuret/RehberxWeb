1184c42a0ab65587c5431f2e87647b05f7c7524e
Murat Demirci <mdemirci@outlook.com>
2018-07-08 15:38:07 +0300

    Proje login bölümü yapıldı. template düzenlendi.

---
+++ src/main/java/com/bilisimegitim/rehberxweb/mb/GirisMB.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bilisimegitim.rehberxweb.mb;
+
+import com.bilisimegitim.rehberxweb.session.GirisFacade;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Bemdell2
+ */
+@Named(value = "girisMB")
+@RequestScoped
+public class GirisMB {
+
+    @EJB
+    private GirisFacade girisFacade;
+
+   
+    private String kullanici;
+    private String sifre;
+    
+    
+    
+    public GirisMB() {
+    }
+
+    public String getKullanici() {
+        return kullanici;
+    }
+
+    public void setKullanici(String kullanici) {
+        this.kullanici = kullanici;
+    }
+
+    public String getSifre() {
+        return sifre;
+    }
+
+    public void setSifre(String sifre) {
+        this.sifre = sifre;
+    }
+    
+    public String giris()
+    {
+        boolean sonuc = girisFacade.girisKontrol(kullanici, sifre);
+        
+        if (sonuc)
+        {
+            // giriş başarılı olduysa, kullanıcı adını session'a yazıyorum.
+            HttpSession session = (HttpSession)FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+            session.setAttribute("username", kullanici);
+            return "menu.xhtml";
+        }
+        else
+        {
+FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Hata", "Kullanıcı adı ya da şifre yanlış!"));
+            return "";
+        }
+        
+        
+    }
+    
+    
+    
+    
+}
